17262dbddedba1eeb327b121b07a27a65396c6fb
UPASANA <UPASANA@Lenovo-PC>
2016-01-05 09:02:52 +0530

    2nd change

--- mvnex-examples/ch-profile/profile/src/main/java/org/sonatype/mavenbook/App.java
+++ mvnex-examples/ch-profile/profile/src/main/java/org/sonatype/mavenbook/App.java
@@ -9,6 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        system.out.println("my name is sree...")
+        system.out.println("my name is sree...");
+        system.out.println("my name is sree...");
+        
     }
 }
